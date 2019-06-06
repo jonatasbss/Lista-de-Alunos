75dc2485699904d79d5e032c9ddb1afc958e91db
Jonatas Batisa <jonatasbssampaio@gmail.com>
2019-06-06 20:32:37 -0300

    atualização

--- js/avaliacao.js
+++ js/avaliacao.js
@@ -1,9 +1,24 @@
 function myFunction(){
 
-    var name = document.getSelection('#name').value;
+    var form = document.getElementById('form-horizontal');
+    var name = document.getElementById('name');
+    var idade = document.getElementById('idade');
+    var email = document.getElementById('email');
+
+
+    form.addEventListener('submit', function(e){
+
+        document.getElementById('nome').innerHTML = name.value;
+        document.getElementById('anos').innerHTML = idade.value;
+        document.getElementById('e-mail').innerHTML = email.value;
+        document.getElementById("resultado").style.visibility = "visible";
+
+        e.preventDefault();
+
+        
+
+    });
+    
     
 
-    document.getSelection('#resultado').innerHTML = name;
-    
-    
 }
